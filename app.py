021bed1e84a4299ee8b0d1812e6f2a2d9120cea0
IT'S ULL4SH <mahbub.ullash01@gmail.com>
2025-09-25 07:17:02 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import httpx
 
 app = FastAPI()
 
-UPSTREAM_API = "http://87.106.101.66:6591"  # <-- replace with the real target API
+UPSTREAM_API = "http://87.106.52.7:6807"  # <-- replace with the real target API
 
 @app.api_route("/{path:path}", methods=["GET", "POST"])
 async def mask_api(path: str, request: Request):
